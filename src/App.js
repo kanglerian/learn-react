28bbf062ef1e8816181fcb8c3b8c8830e1dac22a
kampusinformatika <kanglerian.business@gmail.com>
2022-06-23 17:10:46 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes
+} from 'react-router-dom';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/about' element={ <About/> } />
+        <Route path='/users' element={ <Users/> } />
+      </Routes>
+    </Router>
+  );
+}
+
+const Home = () => {
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
+}
+
+const About = () => {
+  return <h2>About</h2>
+}
+
+const Users = () => {
+  return <h2>Users</h2>
 }
 
 export default App;
